0d8eb877d40cb31e7590f5e36ab9d5aab4756834
Luboš Mátl <matllubos@gmail.com>
2014-12-20 15:45:27 -0600

    Enum datastructure

---
+++ chamber/forms/widgets.py
@@ -0,0 +1,26 @@
+from django.forms.widgets import Widget
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+
+
+class ReadonlyWidget(Widget):
+
+    def __init__(self, widget):
+        super(ReadonlyWidget, self).__init__()
+        self.widget = widget
+
+    def _get_value(self, value):
+        if hasattr(self.widget, 'choices'):
+            print type(self.widget.choices)
+            result = dict(self.widget.choices).get(value)
+        else:
+            result = value
+        return result or ''
+
+    def render(self, name, value, attrs=None, choices=()):
+
+        if isinstance(value, (list, tuple)):
+            out = ', '.join(self._get_value(value))
+        else:
+            out = self._get_value(value)
+        return mark_safe('<p>%s</p>' % conditional_escape(out))
--- chamber/utils/datastructures.py
+++ chamber/utils/datastructures.py
@@ -1,25 +1,31 @@
+from django.utils.datastructures import SortedDict
+
+
+class SmartDict(SortedDict):
+
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        raise AttributeError
+
 
 class Enum(set):
 
-    def __init__(self, *args):
-        super(Enum, self).__init__(args)
+    def __init__(self, *items):
+        super(Enum, self).__init__(items)
 
     def __getattr__(self, name):
         if name in self:
             return name
         raise AttributeError
 
-    @property
-    def choices(self):
-        return [(val, val) for val in self]
 
+class NumeEnum(dict):
 
-class NumericEnum(dict):
-
-    def __init__(self, *args):
-        super(NumericEnum, self).__init__()
+    def __init__(self, *items):
+        super(NumeEnum, self).__init__()
         i = 1
-        for arg in args:
+        for arg in items:
             if arg is not None:
                 self[arg] = i
             i += 1
@@ -29,7 +35,59 @@ class NumericEnum(dict):
             return self[name]
         raise AttributeError
 
+
+class ChoicesEnum(SortedDict):
+
+    def __init__(self, *items):
+        super(ChoicesEnum, self).__init__()
+        for key, val in items:
+            self[key] = val
+
+    def __getattr__(self, name):
+        if name in self:
+            return name
+        raise AttributeError
+
+    def get_label(self, name):
+        if name in self:
+            return self[name]
+        raise AttributeError
+
     @property
     def choices(self):
-        return [(val, key) for key, val in self.items()]
+        return self.items()
 
+
+class ChoicesNumEnum(SortedDict):
+
+    def __init__(self, *items):
+        super(ChoicesNumEnum, self).__init__()
+        i = 0
+        for item in items:
+            if len(item) == 3:
+                key, val, i = item
+                if not isinstance(i, int):
+                    raise ValueError('Last value of item must by integer')
+            elif len(item) == 2:
+                key, val = item
+                i += 1
+            else:
+                raise ValueError('Wrong input data format')
+
+            if i in (j for j, _ in self.values()):
+                raise ValueError('Index %s already exists, please renumber choices')
+            self[key] = (i, val)
+
+    def __getattr__(self, name):
+        if name in self:
+            return self[name][0]
+        raise AttributeError
+
+    def get_label(self, i):
+        if i in dict(self.values()):
+            return self[i]
+        raise AttributeError
+
+    @property
+    def choices(self):
+        return self.values()
