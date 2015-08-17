3360f5d4c3576ec89dfcfab02764bca29da16aa1
Lubos Matl <matllubos@gmail.com>
2015-08-17 14:48:20 +0200

    v0.1.4 Added get_class_method helper

--- chamber/utils/__init__.py
+++ chamber/utils/__init__.py
@@ -3,3 +3,13 @@ import unicodedata
 
 def remove_diacritics(string_with_diacritics):
     return unicodedata.normalize('NFKD', string_with_diacritics).encode('ASCII', 'ignore')
+
+
+def get_class_method(cls_or_inst, method_name):
+    cls = cls_or_inst
+    if not isinstance(cls, type):
+        cls = cls_or_inst.__class__
+    meth = getattr(cls, method_name, None)
+    if isinstance(meth, property):
+        meth = meth.fget
+    return meth
