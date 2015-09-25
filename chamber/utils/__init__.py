ea89fa786151b2426cd3b521dfacbea607973b54
Lubos Matl <matllubos@gmail.com>
2015-09-25 17:38:49 +0200

    0.0.16 Add utilities from version 0.1

---
+++ chamber/utils/__init__.py
@@ -0,0 +1,15 @@
+import unicodedata
+
+
+def remove_accent(string_with_diacritics):
+    return unicodedata.normalize('NFKD', string_with_diacritics).encode('ASCII', 'ignore')
+
+
+def get_class_method(cls_or_inst, method_name):
+    cls = cls_or_inst
+    if not isinstance(cls, type):
+        cls = cls_or_inst.__class__
+    meth = getattr(cls, method_name, None)
+    if isinstance(meth, property):
+        meth = meth.fget
+    return meth
