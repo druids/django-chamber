0598638ce2628dd8506c6f0776d04b321430fb3e
Lubos Matl <matllubos@gmail.com>
2016-02-24 23:09:40 +0100

    v0.1.9 Added keep spacing util

--- chamber/utils/__init__.py
+++ chamber/utils/__init__.py
@@ -1,6 +1,10 @@
 import unicodedata
 
 from django.utils.functional import cached_property
+from django.utils.safestring import mark_safe, SafeData
+from django.utils.text import normalize_newlines
+from django.utils.html import escape
+from django.template.defaultfilters import stringfilter
 
 
 def remove_accent(string_with_diacritics):
@@ -17,3 +21,12 @@ def get_class_method(cls_or_inst, method_name):
     elif isinstance(meth, cached_property):
         meth = meth.func
     return meth
+
+
+def keep_spacing(value, autoescape=True):
+    autoescape = autoescape and not isinstance(value, SafeData)
+    value = normalize_newlines(value)
+    if autoescape:
+        value = escape(value)
+    value = mark_safe(value.replace('  ', ' &nbsp;'))
+    return mark_safe(value.replace('\n', '<br />'))
