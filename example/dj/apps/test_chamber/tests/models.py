5a08da478fc2fa98e449f3ec1f326f277d05ee4b
Lubos Matl <matllubos@gmail.com>
2016-03-14 17:08:54 +0100

    Improved smart model methods and meta

--- example/dj/apps/test_chamber/tests/models.py
+++ example/dj/apps/test_chamber/tests/models.py
@@ -6,10 +6,11 @@ from django.test import TestCase
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 from germanium.tools import assert_equal, assert_raises, assert_true, assert_false
 
-from test_chamber.models import DiffModel, ComparableModel, CleanedSmartModel, NotCleanedSmartModel
+from test_chamber.models import DiffModel, ComparableModel, TestSmartModel
 
 from chamber.models import Comparator
 from chamber.exceptions import PersistenceException
@@ -19,6 +20,40 @@ class NameComparator(Comparator):
 
     def compare(self, a, b):
         return a.name == b.name
+
+
+class TestProxySmartModel(TestSmartModel):
+
+    def clean_name(self):
+        if len(self.name) >= 10:
+            raise ValidationError('name must be lower than 10')
+
+    class Meta:
+        proxy = True
+
+
+class TestPreProxySmartModel(TestSmartModel):
+
+    def _pre_save(self, *args, **kwargs):
+        self.name = 'test pre save'
+
+    def _pre_delete(self, *args, **kwargs):
+        self.name = 'test pre delete'
+
+    class Meta:
+        proxy = True
+
+
+class TestPostProxySmartModel(TestSmartModel):
+
+    def _post_save(self, *args, **kwargs):
+        self.name = 'test post save'
+
+    def _post_delete(self, *args, **kwargs):
+        self.name = 'test post delete'
+
+    class Meta:
+        proxy = True
 
 
 class ModelsTestCase(TestCase):
@@ -57,20 +92,143 @@ class ModelsTestCase(TestCase):
         assert_false(obj1.equals(obj3, comparator))
         assert_false(obj3.equals(obj1, comparator))
 
-    def test_cleaned_smart_model(self):
-        assert_raises(PersistenceException, CleanedSmartModel.objects.create, name=9 * 'a')
-        obj = CleanedSmartModel.objects.create(name=10 * 'a')
-        obj.name = 9 * 'a'
+    def test_smart_model_clean_pre_save(self):
+        assert_raises(PersistenceException, TestProxySmartModel.objects.create, name=10 * 'a')
+        obj = TestProxySmartModel.objects.create(name=9 * 'a')
+        obj.name = 11 * 'a'
         assert_raises(PersistenceException, obj.save)
-        assert_equal(obj.number, 11)
-        obj.name = 12 * 'a'
-        obj.number = 10
-        assert_raises(PersistenceException, obj.save)
-        assert_equal(obj.number, 10)
+        assert_equal(len(TestProxySmartModel.objects.get(pk=obj.pk).name), 9)
+        obj.save(is_cleaned_pre_save=False)
+        assert_equal(len(TestProxySmartModel.objects.get(pk=obj.pk).name), 11)
 
-    def test_not_cleaned_smart_model(self):
-        obj = NotCleanedSmartModel.objects.create(name=9 * 'a')
-        assert_equal(obj.number, 11)
-        obj.number = 10
+    def test_smart_model_clean_post_save(self):
+        class PostSaveTestProxySmartModel(TestProxySmartModel):
+            class Meta:
+                proxy = True
+
+            class SmartMeta:
+                is_cleaned_pre_save = False
+                is_cleaned_post_save = True
+
+        assert_false(PostSaveTestProxySmartModel.objects.filter(name=10 * 'a').exists())
+        assert_raises(PersistenceException, PostSaveTestProxySmartModel.objects.create, name=10 * 'a')
+        assert_true(PostSaveTestProxySmartModel.objects.filter(name=10 * 'a').exists())
+        obj = PostSaveTestProxySmartModel.objects.create(name=9 * 'a')
+        obj.name = 11 * 'a'
         assert_raises(PersistenceException, obj.save)
-        assert_equal(obj.number, 10)
+        assert_equal(len(PostSaveTestProxySmartModel.objects.get(pk=obj.pk).name), 11)
+        obj.name = 12 * 'a'
+        obj.save(is_cleaned_post_save=False)
+        assert_equal(len(PostSaveTestProxySmartModel.objects.get(pk=obj.pk).name), 12)
+
+    def test_smart_model_clean_atomic_post_save(self):
+        class AtomicPostSaveTestProxySmartModel(TestProxySmartModel):
+            class Meta:
+                proxy = True
+
+            class SmartMeta:
+                is_cleaned_pre_save = False
+                is_cleaned_post_save = True
+                is_save_atomic = True
+
+        assert_false(AtomicPostSaveTestProxySmartModel.objects.filter(name=10 * 'a').exists())
+        assert_raises(PersistenceException, AtomicPostSaveTestProxySmartModel.objects.create, name=10 * 'a')
+        assert_false(AtomicPostSaveTestProxySmartModel.objects.filter(name=10 * 'a').exists())
+        obj = AtomicPostSaveTestProxySmartModel.objects.create(name=9 * 'a')
+        obj.name = 11 * 'a'
+        assert_raises(PersistenceException, obj.save)
+        assert_equal(len(AtomicPostSaveTestProxySmartModel.objects.get(pk=obj.pk).name), 9)
+        obj.name = 12 * 'a'
+        obj.save(is_cleaned_post_save=False)
+        assert_equal(len(AtomicPostSaveTestProxySmartModel.objects.get(pk=obj.pk).name), 12)
+
+    def test_smart_model_clean_pre_delete(self):
+        class PreDeleteTestProxySmartModel(TestProxySmartModel):
+            class Meta:
+                proxy = True
+
+            class SmartMeta:
+                is_cleaned_pre_save = False
+                is_cleaned_pre_delete = True
+
+        obj = PreDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        assert_raises(PersistenceException, obj.delete)
+        assert_true(PreDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+        obj = PreDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        obj.delete(is_cleaned_pre_delete=False)
+        assert_false(PreDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+    def test_smart_model_clean_post_delete(self):
+        class PostDeleteTestProxySmartModel(TestProxySmartModel):
+            class Meta:
+                proxy = True
+
+            class SmartMeta:
+                is_cleaned_pre_save = False
+                is_cleaned_post_delete = True
+
+        obj = PostDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        assert_raises(PersistenceException, obj.delete)
+        assert_false(PostDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+        obj = PostDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        obj.delete(is_cleaned_post_delete=False)
+        assert_false(PostDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+    def test_smart_model_clean_atomic_post_delete(self):
+        class AtomicPostDeleteTestProxySmartModel(TestProxySmartModel):
+            class Meta:
+                proxy = True
+
+            class SmartMeta:
+                is_cleaned_pre_save = False
+                is_cleaned_post_delete = True
+                is_delete_atomic = True
+
+        obj = AtomicPostDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        assert_raises(PersistenceException, obj.delete)
+        assert_true(AtomicPostDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+        obj = AtomicPostDeleteTestProxySmartModel.objects.create(name=10 * 'a')
+        obj_pk = obj.pk
+        obj.delete(is_cleaned_post_delete=False)
+        assert_false(AtomicPostDeleteTestProxySmartModel.objects.filter(pk=obj_pk).exists())
+
+    def test_smart_model_pre_save(self):
+        obj = TestPreProxySmartModel.objects.create()
+        assert_equal(obj.name, 'test pre save')
+        obj.name = 10 * 'a'
+        obj.save()
+        assert_equal(obj.name, 'test pre save')
+        assert_true(TestPreProxySmartModel.objects.filter(name='test pre save').exists())
+
+    def test_smart_model_pre_delete(self):
+        obj = TestPreProxySmartModel.objects.create()
+        assert_equal(obj.name, 'test pre save')
+        obj.delete()
+        assert_equal(obj.name, 'test pre delete')
+
+    def test_smart_model_post_save(self):
+        assert_raises(PersistenceException, TestPostProxySmartModel.objects.create)
+        obj = TestPostProxySmartModel.objects.create(name=10 * 'a')
+        assert_equal(obj.name, 'test post save')
+        assert_false(TestPreProxySmartModel.objects.filter(name='test post save').exists())
+        assert_true(TestPreProxySmartModel.objects.filter(name=10 * 'a').exists())
+        obj.save()
+        assert_true(TestPreProxySmartModel.objects.filter(name='test post save').exists())
+        obj.name = 10 * 'a'
+        obj.save()
+        assert_equal(obj.name, 'test post save')
+        assert_false(TestPreProxySmartModel.objects.filter(name='test post save').exists())
+
+    def test_smart_model_post_delete(self):
+        obj = TestPostProxySmartModel.objects.create(name=10 * 'a')
+        assert_equal(obj.name, 'test post save')
+        obj.delete()
+        assert_equal(obj.name, 'test post delete')
