2468e520fa999e74a3476745b6655b8ea5728cb4
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2017-01-05 21:22:26 +0100

    Added tests for get_class_method

--- chamber/utils/__init__.py
+++ chamber/utils/__init__.py
@@ -7,14 +7,18 @@ from django.utils.text import normalize_newlines
 
 
 def remove_accent(string_with_diacritics):
-    "Removes a diacritics from a given string"
+    """
+    Removes a diacritics from a given string"
+    """
     return unicodedata.normalize('NFKD', string_with_diacritics).encode('ASCII', 'ignore').decode('ASCII')
 
 
 def get_class_method(cls_or_inst, method_name):
-    cls = cls_or_inst
-    if not isinstance(cls, type):
-        cls = cls_or_inst.__class__
+    """
+    Returns a method from a given class or instance. When the method doest not exist, it returns `None`. Also works with
+    properties and cached properties.
+    """
+    cls = cls_or_inst if isinstance(cls_or_inst, type) else cls_or_inst.__class__
     meth = getattr(cls, method_name, None)
     if isinstance(meth, property):
         meth = meth.fget
