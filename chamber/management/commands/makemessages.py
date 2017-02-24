bfaa2f9d1ac46869255ef282df1d029b236ac5f5
Luboš Mátl <matllubos@gmail.com>
2017-02-24 15:08:41 +0100

    Added new keywords to makemessges command

---
+++ chamber/management/commands/makemessages.py
@@ -0,0 +1,14 @@
+from django.core.management.commands import makemessages
+
+
+class Command(makemessages.Command):
+    extra_args = [
+        '--keyword=_l',
+        '--keyword=_nl:1,2',
+        '--keyword=_p:1c,2',
+        '--keyword=_np:1c,2,3',
+        '--keyword=_pl:1c,2',
+        '--keyword=_npl:1c,2,3',
+    ]
+
+    xgettext_options = makemessages.Command.xgettext_options + extra_args
