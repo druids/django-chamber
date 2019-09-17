d55a258afbdaeb40cd00850977a0cc3e6ce2096b
matllubos <matllubos@gmail.com>
2019-09-17 13:40:22 +0200

    Fixed init data command

--- chamber/management/commands/initdata.py
+++ chamber/management/commands/initdata.py
@@ -1,21 +1,11 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from django.core.management.commands import loaddata
+from django.core.management import call_command
 
 
-class Command(loaddata.Command):
-
-    missing_args_message = None
-
-    def add_arguments(self, parser):
-        super().add_arguments(parser)
-        actions = []
-        for action in parser._actions:
-            if vars(action)['dest'] != 'args':
-                actions.append(action)
-        parser._actions = actions
+class Command(BaseCommand):
 
     def handle(self, **options):
         if not settings.INITAL_DATA_PATH:
             raise CommandError('Missing "INITAL_DATA_PATH" configuration')
-        super().handle(settings.INITAL_DATA_PATH, **options)
+        call_command('loaddata', settings.INITAL_DATA_PATH, stdout=self.stdout, stderr=self.stderr)
