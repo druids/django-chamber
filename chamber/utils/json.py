74d26decca6f1d76f2b207c7c4c45e6573cd1e5a
Radim Sückr <radim.suckr@gmail.com>
2020-07-08 23:09:21 +0200

    Handle ValueError from DjangoJSONEncoder

--- chamber/utils/json.py
+++ chamber/utils/json.py
@@ -13,4 +13,8 @@ class ChamberJSONEncoder(DjangoJSONEncoder):
             try:
                 return super().default(val)
             except TypeError:
+                # https://github.com/python/cpython/blob/v3.8.3/Lib/json/encoder.py#L160-L180
                 return 'Unserializable value of "{}"'.format(str(type(val)))
+            except ValueError as ex:
+                # https://github.com/django/django/blob/2.2.14/django/core/serializers/json.py#L81-L104
+                return str(ex)
