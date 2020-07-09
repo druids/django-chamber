807d3347f676e3767dc07f47182f0525291b897f
Radim Sückr <radim.suckr@gmail.com>
2020-07-09 11:08:28 +0200

    Remove bytes serialization

--- chamber/utils/json.py
+++ chamber/utils/json.py
@@ -4,17 +4,11 @@ from django.core.serializers.json import DjangoJSONEncoder
 class ChamberJSONEncoder(DjangoJSONEncoder):
 
     def default(self, val):
-        if isinstance(val, bytes):
-            return {
-                '__type__': bytes.__name__,
-                '__value__': [x for x in val],
-            }
-        else:
-            try:
-                return super().default(val)
-            except TypeError:
-                # https://github.com/python/cpython/blob/v3.8.3/Lib/json/encoder.py#L160-L180
-                return 'Unserializable value of "{}"'.format(str(type(val)))
-            except ValueError as ex:
-                # https://github.com/django/django/blob/2.2.14/django/core/serializers/json.py#L81-L104
-                return str(ex)
+        try:
+            return super().default(val)
+        except TypeError:
+            # https://github.com/python/cpython/blob/v3.8.3/Lib/json/encoder.py#L160-L180
+            return 'Unserializable value of "{}"'.format(str(type(val)))
+        except ValueError as ex:
+            # https://github.com/django/django/blob/2.2.14/django/core/serializers/json.py#L81-L104
+            return str(ex)
